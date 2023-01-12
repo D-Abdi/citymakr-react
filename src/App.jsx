ebfa73d46d776c0667eafa02971276375a56446a
Daniel <danielabdi12@gmail.com>
2023-01-12 12:53:02 +0100

    Add read me

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import io from "socket.io-client";
 import React, { useState, useEffect } from "react";
 import Field from "./components/Field/Field";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://citymakrserver.fly.dev/");
 
 const App = () => {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [socketID, setSockedId] = useState(socket.id);
 
   useEffect(() => {
+    
+
     socket.on("connect", () => {
+      console.log("BLABLALB");
       setIsConnected(true);
       setSockedId(socket.id)
     });
@@ -29,6 +32,7 @@ const App = () => {
 
   return (
     <div>
+      <p></p>
       <Field socket={socket} />
     </div>
   );
